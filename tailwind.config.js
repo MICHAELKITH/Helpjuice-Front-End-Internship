42c792c156de1842c5e03072fee73dd207a4f6f5
MICHAELKITH <michaelkithinji27@gmail.com>
2025-04-15 13:04:43 +0300

    Initial setup commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {enabled: true,content: ['./build/**/*.html', './src/**/*.{js,jsx,ts,tsx}']}, 
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
